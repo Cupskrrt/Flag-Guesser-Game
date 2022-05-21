185749052a9c4f91d13ea8168d19615e05cfd8e2
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-21 14:23:03 +0700

    Initial Commit

---
+++ Shared/Flag_GuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_GuesserApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Flag_GuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
